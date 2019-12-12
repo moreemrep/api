32f89048901b80a0594da19463a77fdaf8485e50
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-12 20:23:17 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,19 @@ import { createServer } from 'http'
 import apolloServer from './apollo'
 import { schema } from './graphql/schema'
 
+function cors (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, token, Recaptcha')
+
+  if (req.method === 'OPTIONS') return res.sendStatus(200)
+
+  next()
+}
+
 const app = express()
+
+app.use(cors)
 
 app.get('/graphql/schema', (req, res) => res.send(schema.typeDefs))
 
