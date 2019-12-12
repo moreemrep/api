bf2ee5ff0a5ad11fb78d73f7cb30282e41d33b20
Rafael Santos <rafaelnsantos1@gmail.com>
2019-12-12 19:41:18 -0300

    dataloader types

--- src/loaders/createDataloaders.ts
+++ src/loaders/createDataloaders.ts
@@ -1,11 +1,12 @@
 import { Repositories } from '~/repositories'
 import Dataloader from 'dataloader'
 import { RepublicaDocument } from '~/repositories/mongoose/republica-model'
+import { Model, Document } from 'mongoose'
 
 export function createDataloaders (repositories: Repositories): Loaders {
   const { Republica } = repositories.mongoose.models
 
-  function Loader<T> (model): Dataloader<string, T[]> {
+  function Loader<T extends Document> (model: Model<T>): Dataloader<string, T> {
     return new Dataloader(async ids => (
       await model.find({ _id: { $in: ids } })
     ).sort((a, b) =>
@@ -22,5 +23,5 @@ export function createDataloaders (repositories: Repositories): Loaders {
 }
 
 export interface Loaders {
-  republica: Dataloader<string, any>;
+  republica: Dataloader<string, RepublicaDocument>;
 }
